a2e863d741e6ff2fe83ae7bc70b03f03ce2dc2fa
Gandalf <totallyreallyEMAIL@eatme.io>
2023-03-06 14:43:17 -0800

    Initial commit - WIP

---
+++ instructions.py
@@ -0,0 +1,23 @@
+""""
+Implement the following features:
+
+the board should be stored as a three-element list,
+while each element is another three-element list (the inner lists represent rows)
+so that all of the squares may be accessed using the following syntax:
+
+board[row][column] #done
+
+each of the inner list's elements can contain 'O', 'X', or a digit representing
+the square's number (such a square is considered free)
+the board's appearance should be exactly the same as the one presented in the example. #done
+
+implement the functions defined for you in the editor. -- kinda
+
+Drawing a random integer number can be done by utilizing a
+Python function called randrange(). The example program below
+shows how to use it (the program prints ten random numbers from 0 to 8). #done
+
+Note: the from-import instruction provides access to the randrange
+function defined within an external Python module called random.
+
+"""
---
+++ main.py
@@ -0,0 +1,151 @@
+# tic tac toe
+
+import random
+player_move = 999
+# random value so that the loops in the function will trigger
+past_moves = []
+# hold past moves, they can be checked against move attempts
+# you can not place an X or O in square already used, duh.
+current_player = 'X'
+
+
+def player_x_o():
+    global current_player
+    match current_player:
+        case 'X':
+            current_player = 'O'
+            return
+        case 'O':
+            current_player = 'X'
+            return
+    # flip the X or O marker to the other one
+
+
+def display_board():
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
+    print(f"""
+       *** TIC TAC TOE ***
+    -------------------------
+    |   {grid[0][0]}   |   {grid[0][1]}   |   {grid[0][2]}   |
+    -------------------------
+    |   {grid[1][0]}   |   {grid[1][1]}   |   {grid[1][2]}   |
+    -------------------------
+    |   {grid[2][0]}   |   {grid[2][1]}   |   {grid[2][2]}   |
+    -------------------------
+    """)
+
+
+def enter_move():
+    # TODO change to validate move function so it can be passed the computers move
+    # global vars so that changes here are can be stored outside and recalled again
+    global player_move
+    global past_moves
+    global grid
+    global current_player
+    player_move = 999 # reset so we don't get an infinite loop
+    # asks the user to enter a move
+    while player_move not in range(1, 10):
+        print(f'    *** {current_player} ***')
+        print(f'PAST MOVES = {past_moves}')
+        try:
+            player_move = int(input('Enter the grid number:\n> '))
+        except ValueError:
+            print('Input is not valid.')
+            continue
+        if player_move not in range(1, 10):
+            print('Please enter number between 1 and 9.')
+            continue
+        if str(player_move) in past_moves:
+            print('That square is occupied')
+            player_x_o()
+            # flip X O value an extra time so that the player stays the same
+            continue
+        else:
+            # checks the input, and updates the board according to the user's decision.
+            match player_move:
+                case 1:
+                    grid[0][0] = current_player
+                case 2:
+                    grid[0][1] = current_player
+                case 3:
+                    grid[0][2] = current_player
+                case 4:
+                    grid[1][0] = current_player
+                case 5:
+                    grid[1][1] = current_player
+                case 6:
+                    grid[1][2] = current_player
+                case 7:
+                    grid[2][0] = current_player
+                case 8:
+                    grid[2][1] = current_player
+                case 9:
+                    grid[2][2] = current_player
+
+            past_moves += str(player_move)
+
+
+def make_list_of_free_fields():
+    # The function browses the board and builds a list of all the free squares;
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+    pass  # WHY? I know there is a reason for this; I don't get it
+
+
+def victory_for():
+    # The function analyzes the board's status in order to check if
+    # the player using 'O's or 'X's has won the game
+    # TODO there must be a better way to do this
+
+    def winner():
+        print(f"""
+
+                PLAYER {current_player} WINS!
+
+                """)
+    if grid[0][0] == current_player and grid[0][1] == current_player and grid[0][2] == current_player:
+        winner()
+        return True
+    if grid[0][0] == current_player and grid[1][1] == current_player and grid[2][2] == current_player:
+        winner()
+        return True
+    if grid[0][0] == current_player and grid[1][0] == current_player and grid[2][0] == current_player:
+        winner()
+        return True
+    if grid[0][1] == current_player and grid[1][1] == current_player and grid[2][1] == current_player:
+        winner()
+        return True
+    if grid[0][2] == current_player and grid[1][1] == current_player and grid[2][0] == current_player:
+        winner()
+        return True
+    if grid[0][2] == current_player and grid[1][2] == current_player and grid[2][2] == current_player:
+        winner()
+        return True
+    if grid[1][0] == current_player and grid[1][1] == current_player and grid[1][2] == current_player:
+        winner()
+        return True
+    if grid[2][0] == current_player and grid[2][1] == current_player and grid[2][2] == current_player:
+        winner()
+        return True
+    else:
+        pass
+
+
+def draw_move():
+    comp_move = (random.randrange(0, 9))+1
+    return comp_move
+    # give a random number to be used as the computers move.
+
+
+grid = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+#        row0              row 1              row 2
+
+while True:
+    display_board()
+    enter_move()
+    if victory_for():
+        break
+    player_x_o()
+
+print('THANKS FOR PLAYING.')
+# TODO refactor for playing against computer
